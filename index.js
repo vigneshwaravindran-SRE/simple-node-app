da5e44c469ac69ce0df55a8d159658e734b62408
vigneshwaravindran-SRE <vigneshwaravindran@gmail.com>
2026-06-04 21:13:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,13 @@ const server = http.createServer((req, res) => {
   res.end("Not found\n");
 });
 
+if (require.main === module) {
+  server.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
+}
 server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
 
-module.exports = server; // needed for tests
+module.exports = server;
