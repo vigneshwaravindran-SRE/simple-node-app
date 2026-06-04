a5e533c133a1b3211e15a21ffbccf78a021ae4cc
vigneshwaravindran-SRE <vigneshwaravindran@gmail.com>
2026-06-04 21:14:47 +0530

    Remove duplicate server.listen call

--- index.js
+++ index.js
@@ -26,8 +26,5 @@ if (require.main === module) {
     console.log("Server running on port 3000");
   });
 }
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
 
 module.exports = server;
